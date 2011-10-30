785243c24cc9ee596ae470316f731991cbb35620
Kevin Ngo <ngoke@tardis.(none)>
2011-10-30 16:12:59 -0700

    sliding pagination thing for recent blog posts

---
+++ media/javascripts/sliding-paginator-thing.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    num_pages = 3;
+
+    // show the first page, hide the rest
+    var i = 0;
+    for (i=0; i<=num_pages; i++){
+        $('div.sliding-index-page' + i).hide();
+    }
+    current_page = 0;
+    $('div.sliding-index-page' + current_page).show();
+
+    // previous page
+    $('#sliding-prev-page').click(function(){
+        if (current_page != 0){
+            $('div.sliding-index-page' + current_page).hide('slide', {direction:'right'}, 1000);
+            set
+            current_page--;
+            $('div.sliding-index-page' + current_page).show('slide', {direction:'left'}, 1400);                
+        }
+    });
+
+    // next page
+    $('#sliding-next-page').click(function(){
+        if (current_page != num_pages){
+            $('div.sliding-index-page' + current_page).hide('slide', {direction:'left'}, 1000);
+            current_page++;
+            $('div.sliding-index-page' + current_page).show('slide', {direction:'right'}, 1400);
+        }
+    });
+
+});
